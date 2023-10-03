348e000934e7661a04cd745d525e9acc1af6d150
JoaoRafaelSouza <joao_rafael_souza@live.com>
2023-10-02 23:09:21 -0300

    Ajuste na classe de envio.

--- Models/ContatoModel.php
+++ Models/ContatoModel.php
@@ -4,7 +4,7 @@ namespace Models;
 class ContatoModel{
     public static function enviarFormulario(){
         // $mail = new \Email();
-        $mail = new Email('vps.dankicode.com', 'testes@dankicode.com', 'gui123456', 'Guilherme');
+        $mail = new \Email('vps.dankicode.com', 'testes@dankicode.com', 'gui123456', 'Guilherme');
 
         $mail->addAdress('joao.rafael.quin@gmail.com', 'Joao');
         $mail->formatarEmail(array('assunto'=>'Este é um teste de envio de email!', 'corpo'=>'Aqui é somente um teste de envio de email.'));
